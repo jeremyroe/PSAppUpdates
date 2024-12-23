aeca8e70938b289b35ebf2d1a6f83eea96593aa1
Jeremy Roe <jeremyroe@Jeremys-Laptop.local>
2024-12-23 13:22:55 -0500

    Fix: Correct module import path in PSAppUpdates.psm1

--- PSAppUpdates.psm1
+++ PSAppUpdates.psm1
@@ -1,5 +1,5 @@
 # Import private functions
-Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 | ForEach-Object { . $_.FullPath }
+Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 | ForEach-Object { . $_ }
 
 # Import public functions
-Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 | ForEach-Object { . $_.FullPath } 
+Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 | ForEach-Object { . $_ } 
